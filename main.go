7b404d228832ad89e4a09b40c5517228ba77f6c7
konojunya <junya03dance@gmail.com>
2018-05-14 15:02:55 +0900

    できた

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/nlopes/slack"
 )
 
 func main() {
-	api := slack.New("TOKEN")
-	groups, err := api.GetGroups(false)
-	if err != nil {
-		panic(err)
-	}
+	api := slack.New(os.Getenv("TOKEN"))
+	// params := slack.PostMessageParameters{}
+	// // attachment := slack.Attachment{
+	// // 	Pretext: "some pretext",
+	// // 	Text:    "some text",
+	// // }
+	// // params.Attachments = []slack.Attachment{attachment}
+	// channelID, timestamp, err := api.PostMessage("CAB37830E", "奥信先生まじパネェ", params)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
 
-	for _, g := range groups {
-		fmt.Printf("ID: %s, Name: %s\n", g.ID, g.Name)
+	rtm := api.NewRTM()
+	go rtm.ManageConnection()
+
+	for msg := range rtm.IncomingEvents {
+		switch ev := msg.Data.(type) {
+		case *slack.TeamJoinEvent:
+			fmt.Println("join a new member!")
+			fmt.Println(ev.User.Name)
+		}
 	}
 }
